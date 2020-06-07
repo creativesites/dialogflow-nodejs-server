3c5461da7ff3c9c9fb634d63bda96ba96232922f
creativesites <46081328+creativesites@users.noreply.github.com>
2020-06-07 11:00:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
+const dff = require('dialogflow-fulfillment');
 
 app.get('/', (req, res)=>{
     res.send('We are live')
 });
 app.post('/', (req,res)=>{
-    const agent =
+    const agent = new dff.WebhookClient({
+        request: req,
+        response: res
+    });
 })
 app.listen(3333, ()=>console.log('server is running at port 3333 '));
